3a0b9de239f70437390e93fafca95086f43cb3db
Patrik Olin <enmejl@gmail.com>
2020-07-25 22:04:46 +0200

    changed repl to use compiler and vm

--- repl/repl.go
+++ repl/repl.go
@@ -5,10 +5,10 @@ import (
 	"fmt"
 	"io"
 
-	"github.com/PatrikOlin/monkey_interpreter/evaluator"
 	"github.com/PatrikOlin/monkey_interpreter/lexer"
 	"github.com/PatrikOlin/monkey_interpreter/parser"
-	"github.com/PatrikOlin/monkey_interpreter/object"
+	"github.com/PatrikOlin/monkey_interpreter/compiler"
+	"github.com/PatrikOlin/monkey_interpreter/vm"
 )
 
 const PROMPT = ">> "
@@ -18,9 +18,7 @@ const TABLE_FLIPPED = `
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
-	env := object.NewEnvironment()
-	macroEnv := object.NewEnvironment()
-
+			
 	for {
 		fmt.Fprintf(out, PROMPT)
 		scanned := scanner.Scan()
@@ -38,15 +36,24 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluator.DefineMacros(program, macroEnv)
-		expanded := evaluator.ExpandMacros(program, macroEnv)
-
-		evaluated := evaluator.Eval(expanded, env)
-
-		if evaluated != nil {
-			io.WriteString(out, evaluated.Inspect())
-			io.WriteString(out, "\n")
+		comp := compiler.New()
+		err := comp.Compile(program)
+		if err != nil {
+			fmt.Fprintf(out, "God damnit! Compilation failed, what is it this time? \n %s\n",
+				err)
+			continue
 		}
+
+		machine := vm.New(comp.Bytecode())
+		err = machine.Run()
+		if err != nil {
+			fmt.Fprintf(out, "Woops! Executing bytecode failed:\n %s\n", err)
+			continue
+		}
+
+		lastPopped := machine.LastPoppedStackElem()
+		io.WriteString(out, lastPopped.Inspect())
+		io.WriteString(out, "\n")
 	}
 }
 
